acdd6735663e4f59b6437f364e047dc654455e3d
Dustin Morando <dustinmorando@me.com>
2015-02-24 09:34:45 -0500

    in the middle of fugaring out my views

--- scrips/main.js
+++ scrips/main.js
@@ -9,16 +9,15 @@ var FoodModel = Backbone.Model.extend ({
     name: '',
     description: '',
     price: '',
-},
-
+  }
 });
 
 var FoodCollection = Backbone.Collection.extend ({
-  model: FoodModel,
-
-   Url: 'https://api.parse.com/1/classes/post',
+   url: 'https://api.parse.com/1/classes/post',
+   model: FoodModel,
 
    parse: function(response) {
+     console.log(response);
      return response.results;
    }
 
@@ -41,6 +40,7 @@ var IndexView = Backbone.View.extend({
 });
 
 var MenuView = Backbone.View.extend({
+  tagName: 'li',
 
   template: _.template($('#menu-template').text() ),
 
@@ -50,15 +50,17 @@ var MenuView = Backbone.View.extend({
   }
 });
 
-var CheckoutView = Backbone.View.extend({
+// var CheckoutView = Backbone.View.extend({
+//
+//   template: _.template($('#checkout-template').text() ),
+//
+//   render: function(){
+//     this.$el.html(this.template());
+//     return this;
+//   }
+// });
 
-  template: _.template($('#checkout-template').text() ),
 
-  render: function(){
-    this.$el.html(this.template());
-    return this;
-  }
-});
 
 // -------------
 // Router / Application State
@@ -71,25 +73,39 @@ var AppRouter = Backbone.Router.extend({
     'checkout': 'checkout',
   },
 
+initialize: function() {
+  this.foodCollection = new FoodCollection();
+},
+
 index: function(){
   this.currentView = new IndexView();
   this.currentView.render();
     $('.app-container').html(this.currentView.el);
 
-    this.currentView = new MenuView();
+    this.currentView = new MenuView({collection: this.foodCollection});
     this.currentView.render();
       $('.app-container').append(this.currentView.el);
 
-      this.currentView = new CheckoutView();
-      this.currentView.render();
-        $('.app-container').append(this.currentView.el);
+      // this.currentView = new CheckoutView();
+      // this.currentView.render();
+      //   $('.app-container').append(this.currentView.el);
 },
 
 });
 
+
+
 // -------------
 // Configuration
 // -------------
+$.ajaxSetup ({
+   headers: {
+     "X-Parse-Application-Id": 'vgJoxHtADWuF5g8502Y9CNbpzzH3EG5Fz07dmNuE',
+     "X-Parse-REST-API-Key": 'wjOqR7hA3d9ZrsW9HBVNJuzieRAZtlUH5KGHn9M8'
+   }
+ });
+
+
 
 // -------------
 // Glue code
